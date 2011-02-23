50a1506512f504eb89294e9328fc60d502c84843
wim.paulussen@gmail.com <wim.paulussen@gmail.com@b1f4698e-1431-0410-92e2-872299cb2494>
2011-02-23 17:13:34 +0000

    JSLint for PSA.js
    git-svn-id: https://php-sqlite-admin.googlecode.com/svn@35 b1f4698e-1431-0410-92e2-872299cb2494

--- js/PSA.js
+++ js/PSA.js
@@ -1,33 +1,27 @@
 /* PSA.js */
+/* jslint : 2011.02.23 */
+/*jslint white: true, onevar: true, undef: true, newcap: 
+true, nomen: true, regexp: true, plusplus: true, bitwise: true, 
+browser: true, devel: true, maxerr: 50, indent: 4 */
+
 var PSA = {
 
-	to_text: function(data)
-	{
+	to_text: function (data) {
 		/* TODO : clean up data (newlines etc) */
 		/* when queries include "'" the link does not work -> I replace the "'" to "##"
 		and revert it back here 
 		*/
-		data = data.replace(/##/g,"'");
+		data = data.replace(/##/g, "'");
 		var cop = document.getElementById('to_text');
 		cop.innerHTML = data;
 	},
 
-	really_drop: function(data) {
+	really_drop: function (data) {
 		var answer = confirm('Really drop ' + data + ' ?');
-		if(!answer) {
+		if (!answer) {
 			return false;
 		}
-		return true;
-	},
-
-	/*
-	really_drop_record: function(data) {
-		var answer = confirm('Really drop ' + data + ' ?');
-		if(!answer) {
-			return false;
-		}
-		
 		return true;
 	}
-	*/
-}
+
+};
