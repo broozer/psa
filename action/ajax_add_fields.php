567bb914f970bedd2d9dc02bff12e860f7f1f7e7
Wim Paulussen <wim@asgc.be>
2012-09-13 23:01:10 +0200

    partial finished checks on table add

--- action/ajax_add_fields.php
+++ action/ajax_add_fields.php
@@ -6,9 +6,7 @@ include_once('loader.php');
 // validate received data
 $ajax_sql = new LitePDO('sqlite:../data/base.sqlite');
 
-// var_dump($ajax_sql);
-
-// $req->set('tblname','');
+$error = FALSE;
 
 if($req->get('tblname') === '') {
 	echo 'Table name may not be blank.';
@@ -17,75 +15,98 @@ if($req->get('tblname') === '') {
 		echo '-';
 	} else {
 		// no fields with different table name
-		$q = "DELETE FROM temp_table_fields WHERE tblname <> '".$req->get('tblname')."' ";
-		// $q = "INSERT INTO temp_table_fields (tblname) VALUES ('xxx')";
-		$ajax_sql->qo($q);
-		// var_dump($ajax_sql);
-		 
-		$tblname = $req->get('tblname');
-		$colname = $req->get('colname');
-		$coltype = $req->get('coltype');
-		$colprime = $req->get('colprime');
-		$colnull = $req->get('colnull');
-		$colsize = $req->get('colsize');
-		$coldefault = $req->get('coldefault');
-
+		/**/
+		// check fields with same name
 		
-		$q = "INSERT INTO temp_table_fields (
-			tblname
-			,colname
-			,coltype
-			,colprime
-			,colnull
-			,colsize
-			,coldefault)
-			VALUES (
-			:tblname
-			,:colname
-			,:coltype
-			,:colprime
-			,:colnull
-			,:colsize
-			,:coldefault)";
-		
-		$ajax_sql->binder('tblname',$tblname);
-		$ajax_sql->binder('colname',$colname);
-		$ajax_sql->binder('colprime',$colprime);
-		$ajax_sql->binder('coltype',$coltype);
-		$ajax_sql->binder('colnull',$colnull);
-		$ajax_sql->binder('colsize',$colsize);
-		$ajax_sql->binder('coldefault',$coldefault);
-		
-		$ajax_sql->qo($q);
-		// DEBUG: var_dump($ajax_sql);
-
-		$q = "SELECT * FROM temp_table_fields";
-		$ajax_sql->qo($q);
-		$res = $ajax_sql->fo();
-
-		$tab = '<table>';
-		$tab .= '<thead>
-			<th>name</th>
-			<th>type</th>
-			<th>size</th>
-			<th>prime</th>
-			<th>null</th>
-			<th>default</th>
-			</thead><tbody>';
-		
-		foreach($res as $item) {
-			$tab .= '
-				<tr><td>'.$item->colname.
-				'</td><td>'.$item->coltype.
-				'</td><td>'.$item->colsize.
-				'</td><td>'.$item->colprime.
-				'</td><td>'.$item->colnull.
-				'</td><td>'.$item->coldefault.
-				'</td><td><a href="#" onclick="PSA.tablerow_delete('.$item->id.');"><button>delete</button></a>'.
-				'</td></tr>';
-		}
-		$tab .= '</tbody></table>';
-		echo $tab;
+		// check if colprime already set , if so -> warning
+		if($req->get('colprime') === "1") {
+			$q = "SELECT id FROM temp_table_fields WHERE colprime > 0";
+			echo $q;
+			$ajax_sql->qo($q);
+			$res = $ajax_sql->fo_one();
+			var_dump($res);
+		//}
+		/**/
+			if(!$res) {
+				// void - no colprime set
+			} else {
+				$message = 'colprime_error';
+				$error = true;
+				
+			}
+		} /* else {
+		/**/	
+			$q = "DELETE FROM temp_table_fields WHERE tblname <> '".$req->get('tblname')."' ";
+			// $q = "INSERT INTO temp_table_fields (tblname) VALUES ('xxx')";
+			$ajax_sql->qo($q);
+			// var_dump($ajax_sql);
+			
+			$tblname = $req->get('tblname');
+			$colname = $req->get('colname');
+			$coltype = $req->get('coltype');
+			$colprime = $req->get('colprime');
+			$colnull = $req->get('colnull');
+			$colsize = $req->get('colsize');
+			$coldefault = $req->get('coldefault');
+	
+			
+			$q = "INSERT INTO temp_table_fields (
+				tblname
+				,colname
+				,coltype
+				,colprime
+				,colnull
+				,colsize
+				,coldefault)
+				VALUES (
+				:tblname
+				,:colname
+				,:coltype
+				,:colprime
+				,:colnull
+				,:colsize
+				,:coldefault)";
+			
+			$ajax_sql->binder('tblname',$tblname);
+			$ajax_sql->binder('colname',$colname);
+			$ajax_sql->binder('colprime',$colprime);
+			$ajax_sql->binder('coltype',$coltype);
+			$ajax_sql->binder('colnull',$colnull);
+			$ajax_sql->binder('colsize',$colsize);
+			$ajax_sql->binder('coldefault',$coldefault);
+			
+			$ajax_sql->qo($q);
+			// DEBUG: var_dump($ajax_sql);
+	
+			$q = "SELECT * FROM temp_table_fields";
+			$ajax_sql->qo($q);
+			$res = $ajax_sql->fo();
+	
+			$tab = '<table>';
+			$tab .= '<thead>
+				<th>name</th>
+				<th>type</th>
+				<th>size</th>
+				<th>prime</th>
+				<th>null</th>
+				<th>default</th>
+				</thead><tbody>';
+			
+			foreach($res as $item) {
+				$tab .= '
+					<tr><td>'.$item->colname.
+					'</td><td>'.$item->coltype.
+					'</td><td>'.$item->colsize.
+					'</td><td>'.$item->colprime.
+					'</td><td>'.$item->colnull.
+					'</td><td>'.$item->coldefault.
+					'</td><td><a href="#" onclick="PSA.tablerow_delete('.$item->id.');"><button>delete</button></a>'.
+					'</td></tr>';
+			}
+			$tab .= '</tbody></table>';
+			echo $tab;
+				
+		//}
 		
 	}
 }
