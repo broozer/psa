c9cda2e3bd373c3d07e66e73152570322d97a7bc
Wim Paulussen <wim@asgc.be>
2015-01-08 12:59:36 +0100

    create db - check dir writable

--- module/create_db.php
+++ module/create_db.php
@@ -5,17 +5,23 @@
 * [name] create_db.php
 * [package] psa
 * [since] 2010.10.21
-* [expl] action file !!
-*/
+* [update] 2015.01.08 - check dir writable
+* [expl] creates database (file) if directory is writable 
+/
 
 if($req->get('newdb') == '') {
 	$sessie->setS('psa-error','Name database cannot be blank.');
-} else {	
-	$sql = new LitePDO('sqlite:'
+} else {
+        // check if directory is writable
+        if(!is_writable($sessie->getS('psa-dir'))) {
+            $sessie->setS('psa-error',"Directory is not writeable, database cannot be created.");
+        } else {    
+	    $sql = new LitePDO('sqlite:'
 		.$sessie->getS('psa-dir').'/'
 		.$req->get('newdb').'.'
 		.$sessie->getS('psa-ext').'');
-	$sessie->setS('psa-message','Database "'.$req->get('newdb').'" created.');
+	    $sessie->setS('psa-message','Database "'.$req->get('newdb').'" created.');
+        }        
 }
 header('location: controller.php?cmd=base');
 exit;
